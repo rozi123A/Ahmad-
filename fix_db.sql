b49ab38761ed282a05421ecd3db2ab67e889abf7
rozi123A <likeask59@gmail.com>
2026-05-20 22:22:43 +0100

    cleanup: remove unnecessary file fix_db.sql

--- fix_db.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE telegram_users MODIFY COLUMN today_ads_date VARCHAR(100) NULL;
-ALTER TABLE telegram_users MODIFY COLUMN spins_date VARCHAR(100) NULL;
-ALTER TABLE telegram_users MODIFY COLUMN last_ad_time VARCHAR(100) NULL;
