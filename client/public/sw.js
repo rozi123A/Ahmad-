e4d4caace5c8dbc304670cb8706e0568548ad090
rozi123A <likeask59@gmail.com>
2026-05-21 00:38:59 +0100

    sync: client/public/sw.js

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,7 +1,6 @@
 self.options = {
-      "domain": "5gvci.com",
-      "zoneId": 11035304
-  }
-  self.lary = ""
-  importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
-  
+    "domain": "3nbf4.com",
+    "zoneId": 10996226
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
